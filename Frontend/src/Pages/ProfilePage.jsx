e72a77a26a33ae6c25d99e7c0adcd23a49f26d54
Santhiya-qtp <santhiya.b@sece.ac.in>
2025-07-30 14:48:04 +0530

    frontend folder new

---
+++ Frontend/src/Pages/ProfilePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <>
+      <div className="main-container">
+        <h1>Profile page</h1>
+      </div>
+    </>
+  )
+}
+
+export default ProfilePage
