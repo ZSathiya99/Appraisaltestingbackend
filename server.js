cbe294538ce675684ac6f62c74c0ce66991b063d
Santhiya-qtp <santhiya.b@sece.ac.in>
2025-08-04 11:57:28 +0530

    edited server.js

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const teachingRoutes = require('./routes/teachingRoutes');
+const path = require('path');
+const cors = require('cors');
 
-const loginRoute = require("./routes/loginRoute");
-const uploadEmployeesRoute = require("./routes/uploadEmployees");
-const teachingRoutes = require("./routes/teachingRoutes");
-
-
-dotenv.config();
 const app = express();
+
+connectDB();
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
-
-app.use("/api", loginRoute);
-app.use("/api", uploadEmployeesRoute);
 app.use('/api', teachingRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("MongoDB connected");
-  app.listen(5000, () => {
-    console.log("Server running on port 5000");
-  });
-})
-.catch(err => console.error("DB error", err));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
