2a62da24dfd1482b419babdae5db73f656cd5d71
Santhiya-qtp <santhiya.b@sece.ac.in>
2025-08-25 12:57:49 +0530

    adeded auth errr log

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET || "appraisal_backend";
+const JWT_SECRET = process.env.JWT_SECRET ;
 
 const authenticate = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   console.log("TOKEN", token);
+  console.log("JWT_SECRET", JWT_SECRET);
   if (!token) {
     return res.status(401).json({ message: 'Token missing' });
   }
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: 'Invalid token' });
-
+    console.error("JWT Error:", err.message);
     req.user = user;  
     next();
   });
